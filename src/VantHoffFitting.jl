147dbf7c7948b3f9a83d748eceb22f86c11cd637
Jakob Tómas Bullerjahn <bullerjahn@gmail.com>
2023-04-23 22:59:55 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.33

---
+++ src/VantHoffFitting.jl
@@ -0,0 +1,5 @@
+module VantHoffFitting
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using VantHoffFitting
+using Test
+
+@testset "VantHoffFitting.jl" begin
+    # Write your tests here.
+end
